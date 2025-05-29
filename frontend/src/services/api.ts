35b8862b94ed8952a567921dd9d17a2742bbec5a
kadu art <ricardosantos.ti15@gmail.com>
2025-05-29 16:19:33 -0300

    ajuste endeeco api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,7 +4,7 @@ import { getAuthToken } from './authService';
 // usa o BASE_URL do .env
 export const BASE_URL =
     process.env.NODE_ENV === 'development'
-        ? 'http://167.234.249.6:5000'
+        ? 'http://167.234.249.6:5000/api'
         : '/api';
 
 const API = axios.create({
